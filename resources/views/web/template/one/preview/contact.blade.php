24f149a77679f069069eb3031b719d10cb6341e8
Krzysztof Dargiewicz <k.dargiewicz@gmail.com>
2025-05-13 23:30:20 +0200

    up

--- resources/views/web/template/one/preview/contact.blade.php
+++ resources/views/web/template/one/preview/contact.blade.php
@@ -6,11 +6,9 @@
         session(['captcha_sum' => $a + $b]);
     @endphp
 
-    <div class="w3-display-container w3-content w3-wide w3-padding-top-24" id="home">
-
-        <div class="w3-container w3-padding-64" id="contact">
-            <h1>{{ __('blog.contact.title') }}</h1><br>
-            <p>{{ __('blog.contact.content') }}</p>
+    <div class="w3-content w3-padding-64">
+        <h2 class="w3-center w3-padding-top-48">{{ __('blog.contact.title') }}</h2>
+            <p class="w3-center w3-padding-top-48" style="font-size: 14px; color: #999; display: block; margin-bottom: 6px;">{{ __('blog.contact.content') }}</p>
             <form action="{{ route('contact.send') }}" method="POST">
                 @csrf
                 <p><input class="w3-input w3-padding-16" type="text" name="contact_name"
@@ -19,17 +17,11 @@
                           placeholder="{{ __('blog.contact.your_email') }}"></p>
                 <p><textarea class="w3-input w3-padding-16" name="contact_message"
                              placeholder="{{ __('blog.contact.message') }}" required></textarea></p>
-
-                <p><label for="captcha" style="font-size: 14px; color: #999; display: block; margin-bottom: 6px;">
-                        {{ __('blog.contact.math_question', ['a' => $a, 'b' => $b]) }}
-                    </label>
-                    <input class="w3-input w3-padding-16" type="text" name="captcha" required
-                           placeholder="{{ __('blog.contact.enter_result') }}"></p>
-                <p>
-                    <button class="w3-button w3-light-grey w3-section"
-                            type="submit">{{ __('blog.contact.send_message') }}</button>
-                </p>
+                <p><input class="w3-input w3-padding-16" type="text" name="captcha" required
+                           placeholder="{{ __('blog.contact.math_question', ['a' => $a, 'b' => $b]) }}"></p>
+                <p><button class="w3-button w3-light-grey w3-section"
+                            type="submit">{{ __('blog.contact.send_message') }}</button></p>
             </form>
         </div>
-    </div>
+
 @endsection
